dbd2ae5b97410dc2111cb5e0ce4ae404368a27e3
Safuraa <safuraa576@gmail.com>
2026-01-19 19:10:07 +0530

    Implement GCD function in gcd_of_two_numbers.py

---
+++ gcd_of_two_numbers.py
@@ -0,0 +1,10 @@
+# gcd_of_two_numbers.py
+# Learned topic: GCD
+# Date: <19-01-2026>
+
+def gcd(m,n)
+  cf[]
+  for i in range (1, min(m,n)+1):
+    if (m%i)==0 and (n%i)==0:
+        cf.append(i)
+  return (cf[-1])
